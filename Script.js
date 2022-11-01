36be96f9b622616589e1fd5bede02695d241d543
Scripting <116783050+Scripting360@users.noreply.github.com>
2022-11-01 11:59:43 +0530

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,60 @@
+function myrescal(){
+return ((parseInt(myfn1()+myfn2())*10**parseInt(myfn3()))+myfn4() );
+}
+ function myfn1() {
+            
+            var x = document.getElementById('1stc');
+              
+            for(i = 0; i < x.length; i++) {
+                  
+                if(x[i].type="radio") {
+                  
+                    if(x[i].checked)
+                        
+                             return ( x[i].value) ;
+                }
+            }
+        } 
+        function myfn2() {
+            
+            var x = document.getElementById('2stc');
+              
+            for(i = 0; i < x.length; i++) {
+                  
+                if(x[i].type="radio") {
+                  
+                    if(x[i].checked)
+                        
+                             return ( x[i].value) ;
+                }
+            }
+        } 
+        function myfn3() {
+            
+            var x = document.getElementById('3stc');
+              
+            for(i = 0; i < x.length; i++) {
+                  
+                if(x[i].type="radio") {
+                  
+                    if(x[i].checked)
+                        
+                             return ( x[i].value) ;
+                }
+            }
+        } 
+
+ function myfn4() {
+            
+            var x = document.getElementById('4stc');
+              
+            for(i = 0; i < x.length; i++) {
+                  
+                if(x[i].type="radio") {
+                  
+                    if(x[i].checked)
+                        
+                             return ( x[i].value) ;
+                }
+            }
+        } 
